1abaac81e633563a534277941c28e92a49c62b4a
Lars Wilhelmer <wilhelmer@talon.one>
2024-02-23 13:28:37 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Find Unused Imports in Markdown
+
+This VS Code extension finds unused import statements in Markdown files and marks them as errors.
+
+It's specifically designed to match the syntax of [Docusaurus import statements](https://docusaurus.io/docs/markdown-features/react#importing-markdown).
+
+This means that all import statements without a matching XML tag on the page will be marked as errors.
+
+## Example
+
+In the following Markdown content, the `import MyComponentSource` statement will be marked as an error because it's never used.
+
+```md
+import PartialExample from './_markdown-partial-example.mdx';
+import MyComponentSource from '!!raw-loader!./myComponent';
+import CodeBlock from '@theme/CodeBlock';
+
+Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+
+<CodeBlock language="jsx">{MyComponentSource}</CodeBlock>
+
+Stet clita kasd gubergren, no sea takimata sanctus est <PartialExample name="Sebastien" /> dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+```
---
+++ src/extension.ts
@@ -0,0 +1,67 @@
+import * as vscode from 'vscode';
+
+let gDC: vscode.DiagnosticCollection;
+
+function findAndReportUnusedImports(document: vscode.TextDocument) {
+    const text = document.getText();
+    const lines = text.split('\n');
+    const imports: string[] = [];
+    const tags: string[] = [];
+
+    lines.forEach(line => {
+        const importMatch = line.match(/^import\s+(.*?)\s+from\s+/);
+        if (importMatch) {
+            const importStatement = importMatch[1].trim().replace(/[{,}\s]/g, '');
+            imports.push(importStatement);
+        } else {
+            const tagMatch = line.match(/<([^\s>\/]+)/);
+            if (tagMatch) {
+                const tagName = tagMatch[1];
+                tags.push(tagName);
+            }
+        }
+    });
+
+    const unusedImports = imports.filter(importName => !tags.includes(importName));
+
+    const diagnostics: vscode.Diagnostic[] = unusedImports.map(unusedImport => {
+        let lineNumber = lines.findIndex(line => line.match(`import.*?${unusedImport}.*?from`));
+        if (lineNumber !== -1) {
+            const range = new vscode.Range(lineNumber, 0, lineNumber, lines[lineNumber].length);
+            const diagnostic = new vscode.Diagnostic(
+                range,
+                `Unused import "${unusedImport}"`,
+                vscode.DiagnosticSeverity.Error
+            );
+            return diagnostic;
+        }
+        return null;
+    }).filter(diagnostic => diagnostic !== null) as vscode.Diagnostic[];
+
+    gDC.set(document.uri, diagnostics);
+}
+
+export function activate(context: vscode.ExtensionContext) {
+    if (!gDC) {
+        gDC = vscode.languages.createDiagnosticCollection('unusedImports');
+    }
+    vscode.workspace.onDidOpenTextDocument((document: vscode.TextDocument) => {
+        if (document.languageId === 'markdown') {
+            findAndReportUnusedImports(document);
+        }
+    });
+
+    vscode.workspace.onDidSaveTextDocument((document: vscode.TextDocument) => {
+        if (document.languageId === 'markdown') {
+            findAndReportUnusedImports(document);
+        }
+    });
+
+    vscode.window.onDidChangeActiveTextEditor((editor: vscode.TextEditor | undefined) => {
+        if (editor && editor.document.languageId === 'markdown') {
+            findAndReportUnusedImports(editor.document);
+        }
+    });
+}
+
+export function deactivate() {}
